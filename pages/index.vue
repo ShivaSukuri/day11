1855f23ddd21c74e582bee436ef2369b2792fbfc
ShivaSukuri <shivasukuri2000@gmail.com>
2024-02-26 18:29:36 +0530

    adding filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
  <div>
   <img src="/shop.avif" alt="shop image" class="shop">
-  <!-- <div>
-        <select name="" id="" v-model="categoryInput">
-        <option value="">None</option>
+  <div>
+        <select name="input" id="inputFilter" v-model="categoryInput"   class="py-3 px-4 pe-9 block  border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600">
+        <option value="">All</option>
         <option value="men's clothing">Men's clothing</option>
         <option value="women's clothing">women's clothing</option>
         <option value="jewelery">jewelary</option>
         <option value="electronics">electronics</option>
         </select>
-        <button class="bg-blue-500" @click="filterrdProducts()">Search</button>
-  </div> -->
+        
+  </div>
     <div class="home-container">
      
-      <div v-for="product of products" :key="product.id" class="product-item p-4 bg-white rounded-md shadow-lg">
+      <div v-for="product of filteredProducts" :key="product.id" class="product-item p-4 bg-white rounded-md shadow-lg">
           <button @click="view(product)">
             <p class="font-bold h-20">{{ product.title }}</p>
         <img :src="product.image" alt="Product Image" class="product-image h-40 object-contain ">
@@ -29,7 +29,6 @@
     middleware:["auth"]
   })
     const { data: products } = await useFetch('https://fakestoreapi.com/products');
-    let items=products.value;
     const { logout }=useNuxtApp();
     const userlogged=userLogeedIn();
     let item=product();
@@ -47,7 +46,16 @@
     item.value.push(product);
       navigateTo('/product');
     }
-   
+    const filteredProducts=computed((input)=>{
+      console.log(input)
+      if(categoryInput.value===""){
+        return products.value;
+      }
+      else{
+        return products.value.filter(prod=>prod.category===categoryInput.value);
+
+      }
+    })
   </script>
   
   <style scoped>
@@ -70,7 +78,13 @@
       margin-left: 5vw;
       margin-top: 5px;
     }
-   
+   #inputFilter{
+    width: 450px;
+    margin-left: 35vw;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 2px solid black;
+   }
   
   </style>
   
